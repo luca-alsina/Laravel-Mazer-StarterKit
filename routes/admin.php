3a6176d879ca2a9e606860edc2481a75687221b3
Luca ALSINA <luca@irisio.fr>
2022-10-02 18:09:18 +0200

    Created MainController and implemented dashboard route on them

--- routes/admin.php
+++ routes/admin.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.pages.dashboard');
-})->name('dashboard');
+Route::get('/', 'App\Http\Controllers\Admin\MainController@index')->name('dashboard');
