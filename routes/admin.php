23c9dce40e8799858211809233765d16c586050b
Luca ALSINA <luca@irisio.fr>
2022-10-03 23:17:09 +0200

    Started integration of simple-datatables

--- routes/admin.php
+++ routes/admin.php
@@ -15,3 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\Admin\MainController@index')->name('dashboard');
+
+Route::group([ 'name' => 'users.', 'prefix' => 'users' ], function () {
+
+    Route::get('/', 'App\Http\Controllers\Admin\UserController@index')->name('index');
+/*    Route::get('/create', 'App\Http\Controllers\Admin\UserController@create')->name('create');
+    Route::post('/store', 'App\Http\Controllers\Admin\UserController@store')->name('store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\Admin\UserController@edit')->name('edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\Admin\UserController@update')->name('update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\Admin\UserController@delete')->name('delete');*/
+
+});
