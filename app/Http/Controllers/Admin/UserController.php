bb9855e9262c17be6ff4b14ebec7e68938f51a46
Luca Alsina <luca@irisio.fr>
2022-10-07 16:33:53 +0200

    Added user list in users page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.user.index');
+        $users = User::all();
+
+        return view('admin.pages.user.index', compact('users'));
     }
 }
