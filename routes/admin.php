d7c1d6eb0dda2d915f45ddb710cbc50d2d1186f8
Luca ALSINA <contact@alsinaluca.fr>
2022-11-11 19:04:36 +0100

    Fixed CategoriesController incorrect patch

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
